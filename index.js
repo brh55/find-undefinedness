adc4d03f5d7f45e46a72b0af37f4a86b44121d81
Brandon Him <brh55@drexel.edu>
2017-01-27 02:39:55 -0800

    Add nested structure support

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 'use strict';
 module.exports = o => {
+	const keys = findUndefinedness(o);
+	return (keys === null) ? keys : keys.split(',');
+};
+
+var findUndefinedness = (o, prop) => {
 	const keys = Object.keys(o)
-					.map(key => typeof o[key] === 'undefined' ? key : '')
-					.filter(key => key);
+					.map(key => {
+						// Indicates current position accounting
+						// for nested positioning
+						const current = (prop) ? `${prop}.${key}` : key;
+						if (typeof o[key] === 'object') {
+							return findUndefinedness(o[key], current);
+						}
+						if (typeof o[key] === 'undefined') {
+							return current;
+						}
+						return '';
+					})
+					.filter(key => key)
+					.toString();
 
 	return (keys.length > 0) ? keys : null;
-};
+}
+
--- test.js
+++ test.js
@@ -16,4 +16,29 @@ test('Test find undefindness', t => {
 	};
 
 	t.is(findUndefindness(kitty), null);
+
+	const user = {
+		name: undefined,
+		github: {
+			username: 'brh55',
+			repositories: {
+				personal: 43,
+				private: undefined
+			},
+			followers: {
+				username: 'rightlag',
+				repositories: {
+					personal: 20,
+					private: undefined
+				},
+				stars: 1
+			},
+			stars: 100
+		},
+		emoji: undefined
+	}
+	t.deepEqual(
+		findUndefindness(user),
+		[ "name", "github.repositories.private", "github.followers.repositories.private", "emoji" ]
+	);
 });
